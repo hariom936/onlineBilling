c29ab142d8d9f796e0a34ab413eed467314b1314
hariom936 <hariomverma968@gmail.com>
2023-01-27 20:20:55 +0530

    adding a two state

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Shop = () => {
-  return (
-    <div>
-        <h2> Buy Shoes only 500 Rs</h2>
-        <button className='btn btn-primary mx-2'> - </button>
-        Add itme to the card
-        <button className='btn btn-primary mx-2'> + </button>
-    </div>
-  )
+    return (
+        <div>
+            <h2> Deposite / Withdraw </h2>
+            <button className='btn btn-primary mx-2'> - </button>
+                Update Balance
+            <button className='btn btn-primary mx-2'> + </button>
+        </div>
+    )
 }
 
 export default Shop
